baf16bddd07ce860e78bbe5fa2b4f9587c87c31d
John Pickett <pickett.bill@gmail.com>
2024-08-22 16:01:42 -0230

    teleporter graphic tiles

--- components/object/teleporter.py
+++ components/object/teleporter.py
@@ -1,13 +1,15 @@
 from .domainobject import DomainObject, Stall
+from ..utility import cut_tile
 
 class Teleporter(DomainObject):
     def __init__(self, graphic, position, destination):
         super().__init__()
         # load either image
         if graphic == 'up':
-            self.load_sheet('sprites', 'teleporter', 'teleporter_up.png')
+            self.image = cut_tile(35, 11)
         else:
-            self.load_sheet('sprites', 'teleporter', 'teleporter_down.png')
+            self.image = cut_tile(36, 11)
+        self.rect = self.image.get_rect()
         # sync position state
         self.sync_cell(position)
         # teleporter destination as a map array coordinate
