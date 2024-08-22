6ad65c683b1b98f18971337d22010266b3ab5c82
John Pickett <pickett.bill@gmail.com>
2024-08-22 17:40:55 -0230

    cache images so only one surface for all uses

--- components/utility.py
+++ components/utility.py
@@ -6,17 +6,23 @@ from .gui.guimanager import colours
 # these are filled in during the initializer of Main()
 font_size = None
 font_object = None
-
 # filled in by Main() is the tile sheet tiles are cut out of
 tiles = None
+# graphic size of the tile, squared
 tile_size = 32
+# dictionary to cache images so one surface for one tile position and reused on later calls
+tile_images = {}
 
 def cut_tile(tile):
-    # cut a tile out of the tile sheet and return it as a surface
-    x, y = tile
-    surface = pygame.Surface((tile_size, tile_size), pygame.SRCALPHA)
-    surface.blit(tiles, (0, 0), Rect(x * tile_size, y * tile_size, tile_size, tile_size))
-    return surface
+    # if the tile is cached return that otherwise create the graphic image and cache it
+    if tile in tile_images.keys():
+        return tile_images[tile]
+    else:
+        x, y = tile
+        surface = pygame.Surface((tile_size, tile_size), pygame.SRCALPHA)
+        surface.blit(tiles, (0, 0), Rect(x * tile_size, y * tile_size, tile_size, tile_size))
+        tile_images[tile] = surface
+        return surface
 
 def image_alpha(*names):
     # load, convert with an alpha channel, and return an image surface
