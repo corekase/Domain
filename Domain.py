b523e27ec573d19138a8db9e159b126f869d5b52
John Pickett <pickett.bill@gmail.com>
2025-04-20 19:35:14 -0230

    move mousemotion block to top as it is most common mouse event

--- Domain.py
+++ Domain.py
@@ -230,6 +230,20 @@ class Main:
                     elif event.key == K_F1:
                         # toggle whether coordinates shown are relative or absolute
                         self.coordinate_toggle = not self.coordinate_toggle
+                # mouse movement and dragging
+                elif event.type == MOUSEMOTION:
+                    if self.dragging:
+                        # move the centre of the viewport
+                        x, y = event.rel
+                        self.domain_manager.main_viewport[0] += x
+                        self.domain_manager.main_viewport[1] += y
+                        # keep the physical mouse position within the view surface rect as much as possible
+                        if not self.view_surface_rect.collidepoint(event.pos):
+                            # outside of view surface rect, move physical mouse position to center of view_surface_rect
+                            pygame.mouse.set_pos(self.view_surface_rect.center)
+                    else:
+                        # update the stored mouse position
+                        self.mouse_position = event.pos
                 # mouse buttons
                 elif event.type == MOUSEBUTTONDOWN:
                     x, y = event.pos
@@ -266,21 +280,6 @@ class Main:
                 elif event.type == MOUSEWHEEL:
                     # adjust the zoom level, event.y will either be a -1 or a 1
                     self.domain_manager.set_zoom_delta(event.y)
-                # dragging action
-                elif event.type == MOUSEMOTION:
-                    if self.dragging:
-                        # move the centre of the viewport
-                        x, y = event.rel
-                        self.domain_manager.main_viewport[0] += x
-                        self.domain_manager.main_viewport[1] += y
-                        # keep the physical mouse position within the view surface rect as much as possible
-                        if not self.view_surface_rect.collidepoint(event.pos):
-                            # outside of view surface rect, move physical mouse position to center of view_surface_rect
-                            pygame.mouse.set_pos(self.view_surface_rect.center)
-                    else:
-                        # update the stored mouse position
-                        self.mouse_position = event.pos
-
 
     def update_status(self):
         # update the x and y map indexes for the information panel
