a8ff819695efdc425cc529cd9187cd5ea8780e1a
John Pickett <pickett.bill@gmail.com>
2025-05-07 23:01:56 -0230

    add teleporter animation

--- components/object/teleporter.py
+++ components/object/teleporter.py
@@ -4,7 +4,9 @@ class Teleporter(DomainObject):
     def __init__(self, position, destination):
         super().__init__()
         # load either image
-        self.load_tiles([(35, 11)])
+        self.load_tiles([(35, 11), (36, 11)])
+        # interval between tile images
+        self.interval = 0.4
         # sync position state
         self.sync_cell(position)
         # teleporter destination as a map array coordinate
