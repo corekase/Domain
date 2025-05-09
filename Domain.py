e5276efe883fa85f311cb9d18d6af66a655d4d8a
John Pickett <pickett.bill@gmail.com>
2025-05-09 15:23:16 -0230

    reorder main initialization

--- Domain.py
+++ Domain.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import FULLSCREEN, SCALED
+# importing utility initializes its namespace for its functions
 from components import utility
 from components.utility import file_resource, image_alpha
 from components.scenes.mainmenu import MainMenu
@@ -9,21 +10,21 @@ class Main:
     def __init__(self):
         # initialize pygame
         pygame.init()
-        # set the default font for utility functions
-        utility.font_size = 16
-        utility.font_object = pygame.font.Font(file_resource('fonts', 'Ubuntu', 'Ubuntu-Medium.ttf'), utility.font_size)
-        # load tile sheet for utility functions
-        utility.tiles = pygame.image.load(file_resource('domains', 'ProjectUtumno_full.png'))
         # create main window surface
         self.screen = pygame.display.set_mode((1920, 1080), FULLSCREEN | SCALED, vsync=1)
-        # set the screen in utility
-        utility.screen = self.screen
         # set window caption
         pygame.display.set_caption('Domain')
         # hide system mouse pointer
         pygame.mouse.set_visible(False)
         # set window icon
         pygame.display.set_icon(image_alpha('icon.png'))
+        # set the screen in utility
+        utility.screen = self.screen
+        # set the default font for utility functions
+        utility.font_size = 16
+        utility.font_object = pygame.font.Font(file_resource('fonts', 'Ubuntu', 'Ubuntu-Medium.ttf'), utility.font_size)
+        # load tile sheet for utility functions
+        utility.tiles = pygame.image.load(file_resource('domains', 'ProjectUtumno_full.png'))
 
     def run(self):
         while True:
