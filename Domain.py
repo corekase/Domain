0f265787a89540fa572309afe38d69a15ddb9af0
John Pickett <pickett.bill@gmail.com>
2025-05-11 13:46:49 -0230

    comment

--- Domain.py
+++ Domain.py
@@ -10,7 +10,7 @@ from components.scenes.game import Game
 if os.name == 'nt':
     # fixes graphical scaling issues with Windows
     # to reproduce issue: run on a 4k display with the screen resolution being 1920x1080
-    # with FULLSCREEN with SCALED flags and Windows OS system scaling set to 150%
+    # with FULLSCREEN and SCALED flags and Windows OS system scaling set to 150%
     # without setting DPIAware below
     import ctypes
     ctypes.windll.user32.SetProcessDPIAware()
